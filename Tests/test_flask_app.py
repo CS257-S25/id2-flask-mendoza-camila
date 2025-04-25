f71db6859cd52a3ddcb76a7ea9410892c0381684
Camila <mendozac@carleton.edu>
2025-04-25 18:06:33 -0500

    deleted test

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -6,11 +6,6 @@ class TestGetActivitiesFromSubCategory(unittest.TestCase):
         self.app = app.test_client()
         response = self.app.get('/Personal_Care_Activities/Sleeping', follow_redirects=True)
         self.assertEqual(["Sleeping","Sleeplessness"], response.data)
+        response2 = self.app.get('/Household_Activities/Housework', follow_redirects=True)
+        self.assertEqual(["Interior cleaning","Laundry"], response2.data)
 
-class TestHomepage(unittest.TestCase):
-    def test_homepage(self):
-        self.app = app.test_client()
-        response = self.app.get('/', follow_redirects=True)
-        #self.assertEqual(response.status_code, 200)
-        self.assertIn(b'To see the list of activities, please add to the end of this URL the /category/subcategory." \
-    " For example: /Personal_Care_Activities/Sleeping OR /Household_Activities/Housework', response.data)
