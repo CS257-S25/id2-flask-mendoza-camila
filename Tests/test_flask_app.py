143b4c85b66ec3a0799ec4916772aa241b3b37a3
Camila <mendozac@carleton.edu>
2025-04-25 20:35:01 -0500

    fixed get activities test

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -1,8 +1,12 @@
 import unittest
 import app
+from flask import Flask
+
+
+app = Flask(__name__)
 
 class TestGetActivitiesFromSubCategory(unittest.TestCase):
-    def get_activities_from_subcategory(self):
+    def test_get_activities_from_subcategory(self):
         self.app = app.test_client()
         response = self.app.get('/Personal_Care_Activities/Sleeping', follow_redirects=True)
         self.assertEqual(["Sleeping","Sleeplessness"], response.data)
