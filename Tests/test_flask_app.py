cb095ea16c04dcc7e6f97719056ae0448323adaa
Camila <mendozac@carleton.edu>
2025-04-25 20:50:39 -0500

    wow

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -1,5 +1,6 @@
 import unittest
 import app
+import json
 from flask import Flask
 
 
@@ -9,6 +10,8 @@ class TestGetActivitiesFromSubCategory(unittest.TestCase):
     def test_get_activities_from_subcategory(self):
         self.app = app.test_client()
         response = self.app.get('/Personal_Care_Activities/Sleeping', follow_redirects=True)
+        print("HERE: ")
+        print(response)
         self.assertEqual(["Sleeping","Sleeplessness"], response.data)
         response2 = self.app.get('/Household_Activities/Housework', follow_redirects=True)
         self.assertEqual(["Interior cleaning","Laundry"], response2.data)
