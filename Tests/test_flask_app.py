8a399caf9550216e4a15bc979c5ba0a5985cac7c
Camila <mendozac@carleton.edu>
2025-05-12 15:08:08 -0500

    first test

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -6,11 +6,13 @@ from flask import Flask
 app = Flask(__name__)
 
 class TestGetActivitiesFromSubCategory(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
 
     def test_get_activities_from_subcategory(self):
         '''tests the get_activities_from_subcategory function'''
-        response = app.get('/Personal_Care_Activities/Sleeping', follow_redirects=True)
-        self.assertEqual(['Sleeping','Sleeplessness'], response.data)
-        response2 = app.get('/Household_Activities/Housework', follow_redirects=True)
-        self.assertEqual(["Interior cleaning","Laundry"], response2.data)
+        response = self.client.get('/Personal_Care_Activities/Sleeping', follow_redirects=True)
+        self.assertEqual(['Sleeping','Sleeplessness'], response)
+        response2 = self.client.get('/Household_Activities/Housework', follow_redirects=True)
+        self.assertEqual(["Interior cleaning","Laundry"], response2)
 
